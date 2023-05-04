8280b42b64ab2e25e0d66a287e40d8803e4e8661
sebastiaonogueira <sebastiaonogueira@unitins.br>
2023-05-03 21:02:29 -0300

    criacao de todos os models

--- api_barq/app/admin.py
+++ api_barq/app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from app.models import Produto
+from app.models import Produto,Estabelecimento,Categoria,Mesa,Consumidor
 
 # Register your models here.
 admin.site.register(Produto)
+admin.site.register(Estabelecimento)
+admin.site.register(Categoria)
+admin.site.register(Mesa)
+admin.site.register(Consumidor)
--- api_barq/app/models.py
+++ api_barq/app/models.py
@@ -1,12 +1,41 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    imagem = models.CharField(max_length=5000)
+
+class Estabelecimento(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=500)
+    usuario = models.CharField(max_length=50)
+    senha = models.CharField(max_length=12)
+    email = models.EmailField(max_length=254)
+
 class Produto(models.Model):
     nome = models.CharField(max_length=300)
     descricao = models.CharField(max_length=2000)
-    imagem = models.CharField(max_length=5000, null=True)
+    imagem = models.CharField(max_length=5000)
     preco = models.FloatField()
     promocao = models.IntegerField()
+    Estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.nome
 
+class Mesa(models.Model):
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE)
+
+class Consumidor(models.Model):
+    nome = models.CharField(max_length=50)
+    mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
+    valido_ate = models.DateTimeField(auto_now_add=True)
+
+class Pedido(models.Model):
+    consumidor = models.ForeignKey(Consumidor, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    total = models.FloatField()
+
+class Pedido_produto(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
